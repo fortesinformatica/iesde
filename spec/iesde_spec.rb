7f01efaa64d736efa74178129d401a4f6108df94
Pedro Benevides <pedro.augusto.sb@gmail.com>
2016-02-26 14:21:05 -0300

    tests for iesde.rb

--- spec/iesde_spec.rb
+++ spec/iesde_spec.rb
@@ -5,7 +5,31 @@ describe Iesde do
     expect(Iesde::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "configure" do
+
+    let(:username) { "12345678" }
+    let(:password) { "123456789123456" }
+
+    subject do 
+      Iesde.configure do |config|
+        config.default_username = username
+        config.default_password = password
+      end
+    end
+
+    before { subject }
+
+    it "Has a configuration set" do
+      expect(Iesde.config).to_not be_nil
+    end
+
+    it "Has a username" do
+      expect(Iesde.config.default_username).to eq(username)
+    end
+      
+    it "Has a password" do
+      expect(Iesde.config.default_password).to eq(password)
+    end
   end
+
 end
