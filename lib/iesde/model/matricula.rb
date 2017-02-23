3da771102c83f91910f755265973e5dcab4e9508
Heron Medeiros <heron.medeiros@gmail.com>
2017-02-23 12:03:51 -0300

    [Matricula] Refac

--- lib/iesde/model/matricula.rb
+++ lib/iesde/model/matricula.rb
@@ -9,39 +9,47 @@ module Iesde
 
       def self.buscar
         matriculas_obtidas = Iesde::Api::ObterMatricula.new(:json)
-        
-        matriculinhas = []
+
         matriculas_obtidas.as_json.map do |matricula|
           params = {}
 
           matricula.map { |k,v| params[k.underscore.to_sym] = v }
-          
-          matriculinhas << Matricula.new(*params.values)
-        end
 
-        matriculinhas
+          Matricula.new(*params.values)
+        end
       end
 
       def self.criar params
-        nova_matricula = Iesde::Api::CriarMatricula.new(:json, params).as_json
-        status = nova_matricula["status"]
-        if status == 1 && nova_matricula.has_key?("MatriculaID")
-          Iesde::Model::Matricula.buscar.select do |matriculinha| 
-            matriculinha.matricula_id.to_s == nova_matricula["MatriculaID"].to_s
+        matricula      = Iesde::Api::CriarMatricula.new(:json, params) #.as_json
+
+        if matricula.salvo_com_sucesso?
+          Iesde::Model::Matricula.buscar.select do |mat|
+            mat.matricula_id.to_s == matricula.matricula_id_do_retorno.to_s
           end.first
         else
-          raise Iesde::Error::WSError.new(nova_matricula["msg"])
+          raise Iesde::Error::WSError.new(matricula.msg)
         end
       end
 
+      def self.criar_com_campos_obrigatorios(curso_id, nome, cpf, email, cep, numero)
+        self.criar(
+          'CursoID' => curso_id,
+          'Nome'    => nome,
+          'CPF'     => cpf,
+          'Email'   => email,
+          'CEP'     => cep,
+          'Numero'  => numero
+        )
+      end
+
       def self.alterar_status params
-        api = Iesde::Api::AlterarStatusMatricula.new(:json, params)
-        retorno = api.as_json
-        status = retorno["status"]
-        if status == 1 && retorno.has_key?("MatriculaID")
-          api.sanitize_campo('msg')
+        api      = Iesde::Api::AlterarStatusMatricula.new(:json, params)
+        retorno  = api.as_json
+
+        if api.salvo_com_sucesso?
+          api.msg
         else
-          raise Iesde::Error::WSError.new(retorno["msg"])
+          raise Iesde::Error::WSError.new(api.msg)
         end
       end
 
@@ -58,7 +66,6 @@ module Iesde
           'Situacao'    => 'I'
         })
       end
-
     end
   end
 end
