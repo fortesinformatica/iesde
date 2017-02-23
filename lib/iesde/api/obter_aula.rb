6d28a10471b057fa34da24a8047919a94a3ea96a
Heron Medeiros <heron.medeiros@gmail.com>
2017-02-23 11:59:29 -0300

    [Aula] Fix error when something goes wrong

--- lib/iesde/api/obter_aula.rb
+++ lib/iesde/api/obter_aula.rb
@@ -3,6 +3,7 @@ module Iesde
     class ObterAula < ::Iesde::API
       def initialize(format, opts)
         super(:aulas, format, opts)
+        @response = "[]" if as_json.is_a?(Hash) && tem_chave?("status") && ( status == false )
       end
     end
   end
